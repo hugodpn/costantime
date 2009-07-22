43aca729e96a095f0446deef18b17c5fadd26a63
Hugo Pacini <hpacini@gisworking.com>
2009-07-22 17:11:44 -0300

    Calculate the profit on report.

--- app/models/historic_project.rb
+++ app/models/historic_project.rb
@@ -20,14 +20,4 @@ class HistoricProject < ActiveRecord::Base
   validates_uniqueness_of :historic_date, :scope => "project_id"
 
 
-  def profit_between(from, to)
-    @sum = 0
-    self.project.person_filter(from, to).each do |pp|
-      @sum += pp.person.cost_by_percentage(pp.percentage)
-    end
-
-    income - @sum
-  end
-
-
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -20,22 +20,37 @@ class Project < ActiveRecord::Base
 
   validates_presence_of :name
 
-  def person_filter from, to
+  def person_filter(from, to)
     @per_pro = []
     self.person_projects.collect { |pp|
       @per_pro << pp if pp.date_worked > from && pp.date_worked < to
     }
     @per_pro
   end
+  #TODO: should change the name by person_project_filter
+  alias_method :person_project_filter, :person_filter
 
   def income_between(from, to)
-       historic_project = HistoricProject.find(:first, :conditions => ["project_id = ? and historic_date > ? and historic_date < ?", self.id, from, to])
-       if historic_project
-         historic_project.income
-       else
-         0
-       end
+    historic_project = HistoricProject.find(:first, :conditions => ["project_id = ? and historic_date > ? and historic_date < ?", self.id, from, to])
+    if historic_project
+      historic_project.income
+    else
+      0
+    end
   end
 
+
+  def profit_between(from, to)
+    
+    income = income_between(from, to)
+
+    @cost = 0
+    
+    person_project_filter(from, to).each do |pp|
+      @cost += pp.person.cost_by_percentage(pp.percentage, from, to)
+    end
+
+    income - @cost
+  end
 
 end
