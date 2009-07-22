b010965d764b6bc804f4d2e34dab90fdb11f6d88
Alejandro Eliseche <aeliseche@gisworking.com>
2009-07-22 16:33:40 -0300

    Completed historic_costs funtionality

--- app/models/historic_cost.rb
+++ app/models/historic_cost.rb
@@ -1,3 +1,7 @@
 class HistoricCost < ActiveRecord::Base
   belongs_to :person
+
+  validates_presence_of :cost
+  validates_presence_of :historic_date
+  validates_uniqueness_of :historic_date, :scope => "person_id"
 end
